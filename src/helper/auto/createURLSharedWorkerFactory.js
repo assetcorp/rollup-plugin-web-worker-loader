fb632e9f2fa3652bf78a887612ee5518aeb06da2
Delali <michaelazumah@live.com>
2022-10-27 00:27:43 +0000

    crate failsafe for ssr

--- src/helper/auto/createURLSharedWorkerFactory.js
+++ src/helper/auto/createURLSharedWorkerFactory.js
@@ -3,7 +3,20 @@ import {isNodeJS} from '\0rollup-plugin-web-worker-loader::helper::auto::isNodeJ
 
 export function createURLSharedWorkerFactory(url) {
     if (isNodeJS()) {
-        throw new Error('rollup-plugin-web-worker-loader does not support Shared Worker in Node.JS');
+        // throw new Error('rollup-plugin-web-worker-loader does not support Shared Worker in Node.JS');
+        // Fail silently
+        // This ensures that systems (like NextJS) who support SSR would silently execute SharedWorker
+        // without failing
+        return function WorkerFactory(options) {
+            let SharedWorker = {
+                port: {
+                    start: () => {},
+                    onmessage: () => {},
+                    onmessageerror: () => {},
+                },
+            };
+            return SharedWorker;
+        };
     }
     return browserCreateURLSharedWorkerFactory(url);
 }
